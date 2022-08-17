a45674992b4a411690e87a75095e6216fae6a3df
LaraSudzi <96569964+LaraSudzi@users.noreply.github.com>
2022-08-17 23:24:43 +0300

    work 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,58 @@
     
 
 function calc (){
+
       let mass = +massInput.value; 
       let height = +heightInput.value; 
         
-      let index = mass / (h * h);
+      let index = mass / (height * height);
+
+      index = index * 100;
 
       index = Math.round (index);
+
+      index = index / 100;
+
+      console.log (`индекс: ${index}`);
+
+      
 
       if(index <= 16) {
         alert(`Ваш индекс: ${index},выраженный дефицит массы тела.`);
         console.log('выраженный дефицит массы тела.');
     }
-         resultPlace.innerHTML = index;
+
+    if(index  > 16 && index <= 18.5) {
+        alert(`Ваш индекс: ${index},недостаточная (дефицит) массы тела.`);
+        console.log('недостаточная (дефицит) массы тела.');
+    }
+    
+    if(index  > 18 && index <= 25) {
+        alert(`Ваш индекс: ${index},норма.`);
+        console.log(',норма.');
+    }
+    
+    
+    if(index  > 25 && index <= 30) {
+        alert(`Ваш индекс: ${index},избыточная масса тела (предожирнеи).`);
+        console.log(',избыточная масса тела (предожирнеи)');
+    }    
+    
+    if(index  > 30 && index <= 35) {
+        alert(`Ваш индекс: ${index},Ожирение 1 степени.`);
+        console.log('Ожирение 1 степени.');
+    }  
+    
+    if(index  > 35 && index <= 40) {
+        alert(`Ваш индекс: ${index},Ожирение 2 степени.`);
+        console.log('Ожирение 2 степени.');
+    } 
+    
+    if(index  > 40 ) {
+        alert(`Ваш индекс: ${index},Ожирение 3 степени.`);
+        console.log('Ожирение 3 степени.');
+    } 
+      resultPlace.innerHTML = index;
     } 
 
  

2a910e55861d358da72b4d54383cc1d5d1f15a45
LaraSudzi <96569964+LaraSudzi@users.noreply.github.com>
2022-08-19 13:13:56 +0300

    work5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,47 @@
 
     
+  function action(){
 
-function calc (){
+  let mass = +massInput.value;
+  let height = +heightInput.value;
+  
+  if(height > 3){
+      height = height / 100;  
+  }
+  
+  let index = mass / (height ** 2);
+  index = Math.round(index * 10)/10;
 
-      let mass = +massInput.value; 
-      let height = +heightInput.value; 
-        
-      let index = mass / (height * height);
 
-      index = index * 100;
-
-      index = Math.round (index);
-
-   
-
-      
-
-    if(index <= 16) {
-        Diagnosis.innerHTML =('выраженный дефицит массы тела.');
+     if(index <= 16) {
+         Diagnosis.innerHTML = ('выраженный дефицит массы тела');
     }
 
-    if(index  > 16 && index <= 18.5) {
-       Diagnosis.innerHTML =('недостаточная (дефицит) массы тела.');
-    }
-    
     if(index  > 18 && index <= 25) {
-       Diagnosis.innerHTML =(',норма.');
+      Diagnosis.innerHTML =('норма.');
     }
     
     
     if(index  > 25 && index <= 30) {
-        Diagnosis.innerHTML =(',избыточная масса тела (предожирнеи)');
-    }    
-    
+       Diagnosis.innerHTML =('избыточная масса тела (предожирнеи)');
+    } 
+
     if(index  > 30 && index <= 35) {
-        Diagnosis.innerHTML =('Ожирение 1 степени.');
-    }  
-    
-    if(index  > 35 && index <= 40) {
-         Diagnosis.innerHTML =('Ожирение 2 степени.');
-    } 
-    
-    if(index  > 40 ) {
-       Diagnosis.innerHTML = ('Ожирение 3 степени.');
-    } 
-      resultPlace.innerHTML = index;
+      Diagnosis.innerHTML =('Ожирение 1 степени.');
+   }  
+   
+   if(index  > 35 && index <= 40) {
+       Diagnosis.innerHTML =('Ожирение 2 степени.');
+   } 
+   
+   if(index  > 40 ) {
+     Diagnosis.innerHTML = ('Ожирение 3 степени.');
+   }  
+
+    resultPlace.innerHTML = index;
+
+  
+     
     } 
 
  
